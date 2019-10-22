5a1fc7247f21842dce4d7c47b92cf3871c1d3b30
unohuim <colquhoun.r@gmail.com>
2019-10-22 15:28:47 -0400

    footer

--- resources/views/partials/_footer.blade.php
+++ resources/views/partials/_footer.blade.php
@@ -14,9 +14,8 @@
                     <ul>
                         <li><a href="home">Home</a></li>
                         <li><a href="about">About </a></li>
-                        <li><a href="dog-training">WoofsU</a></li>
+                        <li><a href="daycare">Daycare </a></li>
                         <li><a href="service-areas">Service Areas</a></li>
-                        <li><a href="gallery.html">Gallery</a></li>
                         <li><a href="inquire">Inquire</a></li>
                     </ul>
                 </div>
