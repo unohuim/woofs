33495436d803b716b4762af147b101bb7259aa9f
Rob Colquhoun <robcolquhoun2020@Robs-MacBook-Pro.local>
2020-10-07 13:42:48 -0400

    caninex thank you

--- app/Http/Controllers/InquiryController.php
+++ app/Http/Controllers/InquiryController.php
@@ -19,28 +19,28 @@ class InquiryController extends Controller
 
     public function inquireThankyou()
     {
-    	if( empty( request( 'g-recaptcha-response' ) ) ) return Redirect::back();
+    	// if( empty( request( 'g-recaptcha-response' ) ) ) return Redirect::back();
 
 
-    	$inquiry = new Inquiry;
+    	// $inquiry = new Inquiry;
 
-        $inquiry->subject = "Doggie Inquiry";
-        $inquiry->referer = $_SERVER['HTTP_REFERER'];
-        $inquiry->ip = $_SERVER['REMOTE_ADDR'];
-        $inquiry->date = date('d/m/Y');
-        $inquiry->time = date('H:i:s');
-        $inquiry->petname = request( 'petname' );
-        $inquiry->name = request( 'name' );
-        $inquiry->intersection = request( 'intersection' );
-        $inquiry->page_source = request( 'page_source' );
-        $inquiry->email = request( 'email' );
-        $inquiry->phone = request( 'phone' );
-        $inquiry->message = request( 'message' );
+     //    $inquiry->subject = "Doggie Inquiry";
+     //    $inquiry->referer = $_SERVER['HTTP_REFERER'];
+     //    $inquiry->ip = $_SERVER['REMOTE_ADDR'];
+     //    $inquiry->date = date('d/m/Y');
+     //    $inquiry->time = date('H:i:s');
+     //    $inquiry->petname = request( 'petname' );
+     //    $inquiry->name = request( 'name' );
+     //    $inquiry->intersection = request( 'intersection' );
+     //    $inquiry->page_source = request( 'page_source' );
+     //    $inquiry->email = request( 'email' );
+     //    $inquiry->phone = request( 'phone' );
+     //    $inquiry->message = request( 'message' );
           
-        $user = new User( [ 'name' => 'Woofs Inquiry', 'email' => 'robert@woofs.ca' ] );
+     //    $user = new User( [ 'name' => 'Woofs Inquiry', 'email' => 'robert@woofs.ca' ] );
 
 
-        \Mail::to( $user )->send( new InquiryMail( $inquiry ) );
+     //    \Mail::to( $user )->send( new InquiryMail( $inquiry ) );
 
     	return view( 'inquiry.inquire_thankyou' );
     }
