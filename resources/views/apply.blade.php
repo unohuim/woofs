7a79cef943e7a7af4811be99ebddeb0d73dbff96
Rob Colquhoun <robcolquhoun2020@Robs-MacBook-Pro.local>
2021-08-04 10:11:05 -0400

    changed google sheet on apply

--- resources/views/apply.blade.php
+++ resources/views/apply.blade.php
@@ -18,7 +18,7 @@
 
 @section ( 'page_paragraph' )
     <div class="about-right-para">
-        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSdo6Kvh-WyH694ZvlaP-CsbTzA_EqCchxXo4OnPuc_S__xoZg/viewform?embedded=true" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSc_38NeaGmnxBkKDV4DBzHDF4ZFmFMCtQxmMyP1k0D1VmQ4Cw/viewform?usp=sf_link" width="760" height="500" frameborder="0" marginheight="0" marginwidth="0">Loading...
         </iframe>
     </div>
 @stop
